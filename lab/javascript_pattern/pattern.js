217618f37724755eb0fc3da5bb4fe8ab0cc45321
penas326 <penas326@newschool.edu>
2015-04-02 03:14:05 +0000

    js pattern

---
+++ lab/javascript_pattern/pattern.js
@@ -0,0 +1,21 @@
+
+var dollar = ["$", " $$", "  $$$", "   $$$$", "    $$$$$", "     $$$$$$", "      $$$$$$$", "       $$$$$$$$", "        $$$$$$$$$", "         $$$$$$$$$$", "          $$$$$$$$$$$", "           $$$$$$$$$$$$", "            $$$$$$$$$$$$$"]
+
+for (var index=0; index <=dollar.length; index++){
+	console.log (dollar [index]);
+}
+
+for (var index =dollar.length; index >=0; index--){
+	console.log (dollar [index]);
+}
+
+for (var index=0; index <=dollar.length; index++){
+	console.log (dollar [index]);
+}
+
+for (var index =dollar.length; index >=0; index--){
+	console.log (dollar [index]);
+}
+for (var index=0; index <=dollar.length; index++){
+	console.log (dollar [index]);
+}
