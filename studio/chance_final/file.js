77e0274368e9ee885dee286a08f114d93088bcbb
penas326 <penas326@newschool.edu>
2015-05-05 15:11:13 +0000

    click and stay only choose 5

---
+++ studio/chance_final/file.js
@@ -0,0 +1,20 @@
+Fortune = {
+  selected : 0
+}
+
+$('.fortune-wrapper').click(function() {
+  if (Fortune.selected < 5) {
+    $(this).addClass('keep');
+  }
+  Fortune.selected += 1;
+});
+
+$('.go-button').click(function(event) {
+  event.preventDefault();
+  
+  if (Fortune.selected > 4) {
+    alert('show the fortune...');
+    
+    $('.fortune-wrapper.keep');
+  }
+});
