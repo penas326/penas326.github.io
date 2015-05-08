9a29e0511fd1eefe1f45d1182c51640a35a6bf02
penas326 <penas326@newschool.edu>
2015-05-08 15:57:52 +0000

    styled list and fortune generated list

--- studio/chance_final/file.js
+++ studio/chance_final/file.js
@@ -5,7 +5,7 @@ var Fortune = {
 var responses0 = ['I drank about 700 bottles of wine to handle the rejection.', 'Cooking by newly-married wife. Husband called 911 for murder attempt.','Fight for what you believe in.', 'The days that followed were a chain of struggle.', 'Test me, test me...'];
 var responses1 = ['Born a twin; Graduated only child.', '"How long is forever.” she asked. “Sometimes just a second.” he said.','She came. She went. My life was never the same again.','We never really grow up, we only learn how to act in public.','Some people are like Slinkies … not really good for anything, but you can’t help smiling when you see one tumble down the stairs.']
 var responses2 = ['Torched the haystack. Found the needle.', 'Apple fell down – pull of gravity. I fell down – full of insanity.', 'From torched skyscrapers, men grew wings.', 'As my wife told our daughter we could not get a puppy because they pee all over the place, my son peed on the carpet.', 'Whistle while you work. ']
-var responses3 = ['I didn’t just fall in love. I fell and shattered.', '“Its been an ugly day.” she said and continued. “Tell me something beautiful.” He said her name.', 'Love begins with a smile, grows with a kiss and ends with a tear.', 'd.	Then the story was just like every beautiful love story, the princess married to the prince, then they were living the happy life...', 'To be trustworthy, a man must know when not to listen, when to remain silent and when to speak out.']
+var responses3 = ['I didn’t just fall in love. I fell and shattered.', '“Its been an ugly day.” she said and continued. “Tell me something beautiful.” He said her name.', 'Love begins with a smile, grows with a kiss and ends with a tear.', 'Then the story was just like every beautiful love story, the princess married to the prince, then they were living the happy life...', 'To be trustworthy, a man must know when not to listen, when to remain silent and when to speak out.']
 var responses4 = ['Sorry soldier, shoes sold in pairs.', 'That night, our game of fugitive ended in an actual cop chase.', 'I fell in love with a teacher once in high school. I would google his license plate to see what information I would get.', '"My God," said the Queen, "I am pregnant. I wonder who did it!"', 'She asked why I was breathing so heavily. I wasnt.']
 var responses5 = ['Hospitality: making your guests feel like they’re at home, even if you wish they were.', 'Always stay in a company of a people who are loyal and trustworthy.', 'We are outlaws on the run', 'If that jubilee don’t come….maybe I’ll meet you on the run.', 'Side one or side two?']
 var responses6 = ['They tried to bury us - They didnt realize we are seeds.', 'Nevertheless, he tried a third time.', 'I was on my third glass of whiskey when the stray cat I let in out of the rain began giving birth to kittens on my couch.', 'I asked God for a bike, but I know God doesn’t work that way. So I stole a bike and asked for forgiveness.', 'If anyone ever tells you that your work is unprofessional, remember: amateurs built the Ark and professionals built the Titanic.']
@@ -45,12 +45,14 @@ $('.fortune-wrapper').click(function() {
 
 $('.go-button').click(function(event) {
   event.preventDefault();
-  var pullFrom = $('.fortune-wrapper[data-selectorder="0"]').data("answers");
-  console.log(pullFrom);
-  //var firstresponse = responses + $('.slide-link[data-selectorder="0"]')[Math.floor(Math.random()*)]
-  $(".response-one").html(pullFrom[Math.floor(Math.random()*pullFrom.length)]);
-  
-  if (Fortune.selected > 4) {
+  $('.keep').each(function() {
+    // console.log($(this).data('answers'));
+    var random_num = Math.floor(Math.random() * 5),
+        answer     = eval($(this).data('answers'))[random_num],
+        list       = '<ol>' + answer + '</ol>';
+        
+    $('.text-fortune').prepend(list);
     
-  }
+  });
+
 });
